a0c52f1802cd1aa326028e8ab2b9783514bf16c5
Kelton01 <4keltonbenson@gmail.com>
2023-10-02 18:50:38 -0700

    Update test.py

--- test.py
+++ test.py
@@ -4,12 +4,10 @@ import os
 
 #Limit memory usage on GPU
 gpus = tf.config.experimental.list_physical_devices('GPU')
-gpus
+print(gpus)
 if gpus:
   try:
     for gpu in gpus:
       tf.config.experimental.set_memory_growth(gpu, True)
   except RuntimeError as e:
     print(e)
-
-print(gpus)
