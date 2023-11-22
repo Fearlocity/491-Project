9d0fff2cfc4fa3b376b3d3cea82500edf0ec2036
Kelton01 <4keltonbenson@gmail.com>
2023-11-21 20:55:12 -0800

    data.py can now be used to create models. model.py now predicts images

--- Image_Analyzer/data.py
+++ Image_Analyzer/data.py
@@ -23,7 +23,15 @@ if gpus:
 training_data = r'C:\Users\Kelton2\Desktop\school stuff\cpsc 491\491-Project\Image_Analyzer\flowers' 
 validation_data = r'C:\Users\Kelton2\Desktop\school stuff\cpsc 491\491-Project\Image_Analyzer\validation_images' 
 
-training_datagen = ImageDataGenerator(rescale = 1./255)
+training_datagen = ImageDataGenerator(
+      rescale = 1./255,
+	    rotation_range=40,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      shear_range=0.2,
+      zoom_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
 
 train_generator = training_datagen.flow_from_directory(
   training_data,
@@ -42,31 +50,32 @@ validation_generator = validation_datagen.flow_from_directory(
 
 
 
-model = Sequential()
-# First Convolution
-model.add(Conv2D(64, (3,3), activation='relu', input_shape=(150,150,3)))
-model.add(MaxPooling2D(2, 2))
-# Second Convolution
-model.add(Conv2D(64, (3,3), 1, activation='relu'))
-model.add(MaxPooling2D(2, 2))
-# Third Convolution
-model.add(Conv2D(128, (3,3), 1, activation='relu'))
-model.add(MaxPooling2D(2, 2))
-# Fourth Convolution
-model.add(Conv2D(128, (3,3), 1, activation='relu'))
-model.add(MaxPooling2D(2, 2))
-# Flatten the results
-model.add(Flatten())
-# 512 neuron hidden layer
-model.add(Dense(512, activation='relu'))
-model.add(Dense(2, activation='softmax')) # This will increase as we add more flowers
-
-model.compile(
-  loss = 'categorical_crossentropy',
-  optimizer='rmsprop',
-  metrics=['accuracy'])
-
-history = model.fit(train_generator, epochs=5, steps_per_epoch=2, validation_data = validation_generator, verbose = 1, validation_steps=3)
+model = tf.keras.models.Sequential([
+    # Note the input shape is the desired size of the image 150x150 with 3 bytes color
+    # This is the first convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The third convolution
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fourth convolution
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dropout(0.5),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(3, activation='softmax') # This value corresponds to the number of flowers the model will support
+])
 
 
-# model.save("flowers.h5")
+model.summary()
+
+model.compile(loss = 'categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+history = model.fit(train_generator, epochs=8, steps_per_epoch=7, validation_data = validation_generator, verbose = 1, validation_steps=3)
+model.save("flowers.keras")
--- Image_Analyzer/model.py
+++ Image_Analyzer/model.py
@@ -1,5 +1,13 @@
+import tensorflow as tf
 from tensorflow.keras.models import load_model
+import keras.utils as image
+import numpy as np
 
-model = load_model(r'C:\Users\Kelton2\Desktop\school stuff\cpsc 491\491-Project\Image_Analyzer\flowers.h5' )
-test = model.predict(r'C:\Users\Kelton2\Desktop\school stuff\cpsc 491\491-Project\Image_Analyzer\validation_images\rose.jpg' )
+model_file = r'C:\Users\Kelton2\Desktop\school stuff\cpsc 491\491-Project\Image_Analyzer\flowers.keras'
+image_file = r'C:\Users\Kelton2\Desktop\school stuff\cpsc 491\491-Project\Image_Analyzer\validation_images\rose\rose.jpg'
+image = tf.keras.preprocessing.image.load_img(image_file, target_size=(150, 150))
+input_arr = tf.keras.preprocessing.image.img_to_array(image)
+input_arr = np.array([input_arr])
+model = tf.keras.models.load_model(filepath = model_file)
+test = model.predict(input_arr)
 print(test)
